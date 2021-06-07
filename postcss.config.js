84240da5742a63476138914e3846c4782608decd
liujuncai <1769031928@qq.com>
2021-06-07 15:35:12 +0800

    feat: add resume md

---
+++ postcss.config.js
@@ -0,0 +1,9 @@
+// const RelaxedUnit = require('postcss-relaxed-unit')
+
+// module.exports = {
+//   plugins: [
+//     RelaxedUnit({
+//      rules: { rx: 'div(100).unit(rem)' },
+//     })
+//   ]
+// }
---
+++ src/views/resume.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <div v-html="htmlContent"></div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "",
+  props: {},
+  data() {
+    return {
+      htmlContent: "",
+    };
+  },
+  components: {},
+  methods: {},
+  mounted() {
+    this.htmlContent = this.$converter.makeHtml("../assets/resume.md");
+  },
+  computed: {},
+  watch: {},
+};
+</script>
+
+<style lang="scss" scoped></style>
